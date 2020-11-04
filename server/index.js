0e755d6e362684cea21696929861aa305c0086f0
millasmar <marcosmillas@gmail.com>
2020-11-04 11:40:10 -0300

    ruta register y login creadas

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const passport = require("passport")
-const LocalStrategy = require('passport-local').Strategy;
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 const db = require("./db/db");
-const {User} = require("./db/models")
+const { User } = require("./db/models");
 
 const app = express();
 const routes = require("./routes");
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(cookieParser()); // popula req.cookie
-app.use(session({ secret: "bootcamp" })) // popula req.session
+app.use(session({ secret: "bootcamp" })); // popula req.session
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -29,7 +29,7 @@ passport.use(
       passwordField: "password",
     },
     function (email, password, done) {
-      User.findOne({ where: {email}})
+      User.findOne({ where: { email } })
         .then((user) => {
           if (!user) {
             return done(null, false); // user not found
@@ -47,20 +47,19 @@ passport.use(
 );
 
 // How we save the user
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
 // How we look for the user
-passport.deserializeUser(function(id, done) {
-  User.findByPk(id)
-    .then(user => done(null, user))
+passport.deserializeUser(function (id, done) {
+  User.findByPk(id).then((user) => done(null, user));
 });
 
 app.use("/api", routes); //todas las rutas empiezan con api
 app.use("/", (req, res, next) => res.redirect("/api")); // me aseguro que si o si vaya para /api si entraste en otra ruta
 
-db.sync({ force: true }).then(() =>
+db.sync({ force: false }).then(() =>
   app.listen(1337, (req, res, next) => {
     console.log("API on port 1337");
   })
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
+let userRoutes = require("./user");
 
-
+router.use("/user", userRoutes);
 
 module.exports = router;
