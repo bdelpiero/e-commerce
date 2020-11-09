06b6606660c4220b99cfc9270ecc3132aa81724a
Bruno Del Piero <bdelpiero93@gmail.com>
2020-11-09 12:27:22 -0300

    mi comit

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {Category} = require("../db/models");
+const { Category } = require("../db/models");
 
-
-router.post("/", (req, res, next)=>{
-    Category.create(req.body)
-        .then(()=> res.sendStatus(201))
+router.post("/", (req, res, next) => {
+  Category.create(req.body).then(() => res.sendStatus(201));
 });
 
-module.exports = router
+router.post("/", (req, res, next) => {
+  Category.findAll().then((data) => res.send(data));
+});
+
+module.exports = router;
