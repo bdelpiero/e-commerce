c8b02da9e6b775f2ece20d0ce665043528834677
Ian <serian2010@hotmail.com>
2020-11-09 12:26:35 -0300

    ruta get categories

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -5,8 +5,15 @@ const {Category} = require("../db/models");
 
 
 router.post("/", (req, res, next)=>{
+  console.log(req.body);
     Category.create(req.body)
         .then(()=> res.sendStatus(201))
+
 });
+
+router.get("/", (req,res)=>{
+  Category.findAll()
+          .then(data => res.send(data))
+})
 
 module.exports = router
